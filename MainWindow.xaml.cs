5ab03162f191e6cdd9804f1ac8deb42038d3a5fb
0ne_Up <oneup1@gmail.com>
2020-06-09 11:39:31 +0200

    - Drag and drop disks between rods.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Hanoi
 {
@@ -20,8 +11,6 @@ namespace Hanoi
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Label dragDisk;
-
         public MainWindow()
         {
             InitializeComponent();
@@ -34,10 +23,10 @@ namespace Hanoi
             for (int i = 1; i <= 8; i++)
             {
                 Label disk = new Label();
-                //disk.Width = 100;
+                disk.HorizontalContentAlignment = HorizontalAlignment.Center;
+                disk.Margin = new Thickness(2);
                 disk.BorderBrush = Brushes.Black;
                 disk.BorderThickness = new Thickness(1);
-                disk.HorizontalContentAlignment = HorizontalAlignment.Center;
                 disk.Content = i;
                 disk.MouseDown += Disk_MouseDown;
                 spRod1.Children.Add(disk);
@@ -46,51 +35,24 @@ namespace Hanoi
 
         private void Disk_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            dragDisk = (Label)sender;
-            DragDrop.DoDragDrop(dragDisk, dragDisk.Content, DragDropEffects.Move);
+            DragDrop.DoDragDrop((Label)sender, sender, DragDropEffects.Move);
         }
 
-        private void spRod1_PreviewDrop(object sender, DragEventArgs e)
+        private void Rod_Drop(object sender, DragEventArgs e)
         {
-        }
+            Console.WriteLine("Drop");
 
-        private void spRod1_Drop(object sender, DragEventArgs e)
-        {
-            Console.WriteLine("Drop on 1");
-            StackPanel rod = (StackPanel)dragDisk.Parent;
-            if (rod != spRod1)
+            Label disk = (Label)e.Data.GetData(typeof(Label));
+            StackPanel sourceRod = (StackPanel)disk.Parent;
+
+            StackPanel targetRod = (StackPanel)((Grid)((Border)sender).Child).Children[1];
+            
+            if (targetRod != sourceRod)
             {
-                rod.Children.Remove(dragDisk);
-                spRod1.Children.Add(dragDisk);
-                Console.WriteLine("Moved to 1");
+                sourceRod.Children.Remove(disk);
+                targetRod.Children.Insert(0, disk);
+                Console.WriteLine("Moved");
             }
-            dragDisk = null;
-        }
-
-        private void spRod2_Drop(object sender, DragEventArgs e)
-        {
-            Console.WriteLine("Drop on 2");
-            StackPanel rod = (StackPanel)dragDisk.Parent;
-            if (rod != spRod2)
-            {
-                rod.Children.Remove(dragDisk);
-                spRod2.Children.Add(dragDisk);
-                Console.WriteLine("Moved to 2");
-            }
-            dragDisk = null;
-        }
-
-        private void spRod3_Drop(object sender, DragEventArgs e)
-        {
-            Console.WriteLine("Drop on 3");
-            StackPanel rod = (StackPanel)dragDisk.Parent;
-            if (rod != spRod3)
-            {
-                rod.Children.Remove(dragDisk);
-                spRod3.Children.Add(dragDisk);
-                Console.WriteLine("Moved to 3");
-            }
-            dragDisk = null;
         }
     }
 }
